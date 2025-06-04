cb04f2373d21fec22ab8bf03ae44896cd71aba8f
ThetNaingSoe <thetnaingsoe5163@gmail.com>
2025-06-04 19:36:23 +0700

    Implementing category service

--- ordermanagement/src/main/java/com/tns/ordermanagement/model/repo/admin/CategoryRepo.java
+++ ordermanagement/src/main/java/com/tns/ordermanagement/model/repo/admin/CategoryRepo.java
@@ -1,8 +1,11 @@
 package com.tns.ordermanagement.model.repo.admin;
 
+import java.util.Optional;
+
 import com.tns.ordermanagement.model.entity.Category;
 import com.tns.ordermanagement.model.repo.BaseRepo;
 
 public interface CategoryRepo extends BaseRepo<Category, Integer> {
 
+	Optional<Category> findByName(String name);
 }
--- ordermanagement/src/main/java/com/tns/ordermanagement/service/admin/CategoryService.java
+++ ordermanagement/src/main/java/com/tns/ordermanagement/service/admin/CategoryService.java
@@ -3,6 +3,8 @@ package com.tns.ordermanagement.service.admin;
 import org.springframework.stereotype.Service;
 
 import com.tns.ordermanagement.controller.admin.dto.NewCategoryForm;
+import com.tns.ordermanagement.exception.AppBusinessException;
+import com.tns.ordermanagement.model.entity.Category;
 import com.tns.ordermanagement.model.repo.admin.CategoryRepo;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,15 @@ public class CategoryService {
 	private final CategoryRepo repo;
 	
 	public void addNewCategory(NewCategoryForm form) {
+		var oldCategory = repo.findByName(form.getName());
 		
+		if(!oldCategory.isEmpty()) {
+			throw new AppBusinessException("Category already exists.");
+		}
+		
+		var newCategory = new Category();
+		newCategory.setName(form.getName());
+		
+		repo.save(newCategory);
 	}
 }
